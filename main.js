d35c69a944a522ca85645c44e5b84d6e1d633a66
kaysarulkanak <kanakece2016@gmail.com>
2020-08-06 20:05:46 +0600

    code updated

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 const fs = require('fs')
 fs.readFile("./test.txt", 'utf8',(err, data)=>{
-    if(data){
-        console.log(data)
-    }
-
     if(err){
         console.log(err)
+    }else{
+        console.log(data)
     }
 });
 fs.readFile("./test1.txt", 'utf8',(err,data)=>{
-    if(data){
-        fs.appendFile('./test1.txt', ' Hello World', ()=>{})
-    }
-
     if(err){
         console.log(err)
+    }else{
+        fs.appendFile('./test1.txt', ' Hello World', ()=>{})
     }
 });
 
